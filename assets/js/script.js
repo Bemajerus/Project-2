1befbe9a8d39c87500be9b956040862a48143c7c
Bemajerus <bertrandmajerus@yahoo.com.br>
2023-12-12 22:29:59 +0000

    Create modal popup (html, css and js) & questions.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,182 +1,31 @@
-const questions = [
-    {
-        question: "Are you under 18?",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: "You cannot donate at this time. However, we would be delighted to see you when you are 18."
-    },
-    {
-        question: "Are you 65 years of age or over?",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: "Donors over 70/plus must have a GP letter stating fitness to donate (this needs to be renewed annually), have donated within the last two years and are otherwise eligible. The maximum age to give blood is 79 years. If you are over 65 and have never given blood before, then you cannot donate However:  - if you are between 65 and 69 years and have given blood in the last 10 years then you can give blood."
-    },
-    {
-        question: "Do you weigh less than 7 stone 12 lbs (less than 50kgs) or more than 25 stone (158kgs)?",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: "If you weigh less than 50 kgs or over 158 kgs you are unable to donate at this time."
-    },
-    {
-        question: "For females (including transgender females) and transgender males only: Are you a first time donor or under 26 or have you not donated in 5 years?",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: "Yes - If you are a female (including transgender females) or transgender male donor , donating for the first time or you have not donated in 5 years or you are under 26 years of age and are less than 5ft 6 inches (168cms) in height and less than 10st 3lb (65 kgs), your height and weight will need to be assessed to establish your eligibility to donate."
-    },
-    {
-        question: "Are you currently taking any medication other than the pill or HRT for the menopause? This includes prescribed or over the counter painkillers, aspirin, anti-inflammatories and creams, gels, or drops",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: "Some medications can be harmful to a patient receiving a blood transfusion.  You may not be be able to donate if you are taking medication to treat an illness or infection.In general, you must be fully recovered from an illness or infection and have completed the course of medication before donating.Please note, not all medications prevent donation -  please contact us at 1800 731137 for further information. Please ensure that you know the name of any medication you are currently taking or have taken in the past 4 weeks so that we can can assess your eligibility to donate."
-    },
-    {
-        question: "Today or in the last 2 weeks have you had any illness or injury, e.g. a cough, a cold, a sore throat, vomiting, diarrhoea or an open wound?",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: "You cannot donate if you have any illness or injury which may mean that it is not safe to give your blood to a sick patient. Please check the FAQs or contact us on 1800 731 137 so that we can advise you."
-    },
-    {
-        question: "Have you seen a doctor or had any medical tests or investigations in the last 12 months e.g. xrays , scans and or endoscopy?",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-    {
-        question: "",
-        options: ["Yes", "No"],
-        correctAnswer: "No",
-        explanation: ""
-    },
-]
+import questions from "./questions.js";
 
 let currentQuestionIndex = 0;
 
+// Button to open the modal
+const openModalButton = document.getElementById('openModalButton');
+// Modal
+const questionModal = document.getElementById('questionModal');
+// Button to close the modal
+const closeModalButton = document.getElementById('closeModalButton');
+
+// Add an event listener for closing the modal
+closeModalButton.addEventListener('click', () => {
+    // Reset the question index when the modal is closed
+    currentQuestionIndex = 0;
+    questionModal.style.display = 'none';
+    showQuestion(); // Show the first question when the modal is reopened
+});
+
+// Add an event listener for clicking the button
+openModalButton.addEventListener('click', () => {
+    // Reset the question index when the modal is closed
+    currentQuestionIndex = 0;
+    questionModal.style.display = 'block';
+    showQuestion(); // Show the first question when the modal is reopened
+});
+
+// Function to show the current question
 function showQuestion () {
     const questionContainer = document.getElementById('questions');
     const optionsContainer = document.getElementById('options');
@@ -198,6 +47,7 @@ function showQuestion () {
     explanationContainer.textContent = "";
 }
 
+// Function to check the user's answer
 function handleAnswer(userAnswer) {
     const currentQuestion = questions[currentQuestionIndex];
 
@@ -208,13 +58,10 @@ function handleAnswer(userAnswer) {
         if (currentQuestionIndex < questions.length) {
             showQuestion();
         } else {
-            document.getElementById('questionContainer').innerHTML = "<h2>Thank you for answering all questions! You can donate blood!</h2>";
+            document.getElementById('questionContainer').innerHTML = "<h2>Thank you for answering all questions!</h2>";
         }
     } else {
         const explanationContainer = document.getElementById('explanation');
         explanationContainer.textContent = currentQuestion. explanation;
     }
 }
-
-// Show the first question on page load
-showQuestion();
